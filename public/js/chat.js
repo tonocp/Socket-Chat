ebec9d9e764af4b15fee3372be62c5243ee86134
Tono_JIG <acarrascosa@jig.es>
2023-05-10 13:28:02 +0200

    JWT Validations

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,63 @@
-const socket = io();
+let url = window.location.hostname.includes('localhost')
+  ? 'http://localhost:8080/api/auth/'
+  : 'https://restserver-curso-fher.herokuapp.com/api/auth/';
+
+let usuario = null;
+let socket = null;
+
+// HTML REFS
+const txtUid = document.querySelector('#txtUid');
+const txtMensaje = document.querySelector('#txtMensaje');
+const ulUsuarios = document.querySelector('#ulUsuarios');
+const ulMensajes = document.querySelector('#ulMensajes');
+const btnSalir = document.querySelector('#btnSalir');
+
+const validarJWT = async () => {
+  const token = localStorage.getItem('token') || '';
+  if (token.length <= 10) {
+    window.location = 'index.html';
+    throw new Error('No hay token en el servidor');
+  }
+  const resp = await fetch(url, {
+    headers: { 'x-token': token },
+  });
+  const { usuario: userDB, token: tokenDB } = await resp.json();
+  localStorage.setItem('token', tokenDB);
+  usuario = userDB;
+
+  await conectarSocket();
+};
+
+const conectarSocket = async () => {
+  socket = io({
+    extraHeaders: {
+      'x-token': localStorage.getItem('token'),
+    },
+  });
+
+  socket.on('connect', () => {
+    console.log('Sockets Online');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Sockets Offline');
+  });
+
+  socket.on('recibir-mensajes', () => {
+    // TODO:
+  });
+
+  socket.on('usuarios-activos', () => {
+    // TODO:
+  });
+
+  socket.on('mensaje-privado', () => {
+    // TODO:
+  });
+};
+
+const main = async () => {
+  await validarJWT();
+};
+
+main();
--- sockets/controller.js
+++ sockets/controller.js
@@ -1,9 +1,12 @@
 const { Socket } = require('socket.io');
+const { comprobarJWT } = require('../helpers');
 
-const socketController = (socket = new Socket()) => {
-  /**
-   *
-   */
+const socketController = async (socket = new Socket()) => {
+  const usuario = await comprobarJWT(socket.handshake.headers['x-token']);
+  if (!usuario) {
+    return socket.disconnect();
+  }
+  console.log(`${usuario.nombre} se ha conectado`);
 };
 
 module.exports = {
